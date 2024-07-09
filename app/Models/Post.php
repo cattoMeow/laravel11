71937d5ebb4baa188c2935a589937c20ec8275f4
cattoMeow <61348285+cattoMeow@users.noreply.github.com>
2024-07-09 11:36:19 +0700

    Revert "fixing duplicate fillable column"
    This reverts commit 8112502cc2a1dae36ae56fa2160113f6de49c346.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     // protected $table = 'blog_posts';
-    protected $fillable = ['title', 'author', 'slug', 'body'];
+    protected $fillable = ['title', 'author_id', 'slug', 'body'];
     use HasFactory;
 }
