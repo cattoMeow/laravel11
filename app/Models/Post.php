ccc2725f5169a6d76e6f0649c060b91cd5e6f617
cattoMeow <61348285+cattoMeow@users.noreply.github.com>
2024-08-15 21:23:23 +0700

    first attempt of search function

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -18,11 +19,20 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
-    
+
     // 1 post punya 1 kategori (one to one)
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
-        
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        // if (isset($filters['search']) ? $filters['search'] : false) {
+        // if ($filters['search'] ?? false) {
+        $query-> when($filters['search'] ?? false, function($query, $search){
+            
+            $query->where('title', 'like', '%' . request('search') . '%');
+        }); 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,8 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    // $posts = Post::with(['author', 'category'])->latest()->get();
-    $posts = Post::latest()->get();
-    
-    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search']))->latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -33,13 +31,13 @@ Route::get('/contact', function () {
 
 Route::get('/authors/{user:username}', function (User $user) {
     // $posts = $user -> posts-> load('category', 'author');
-    
+
     return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 Route::get('/categories/{category:slug}', function (Category $category) {
     // $posts = $category->posts->load('category', 'author');
-    
-    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category -> posts]);
+
+    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 // Route::get('/authors', function(){
